8aad9a2c297a1ec50c93b25497c27b72f0b0a825
Kid <kidonng@gmail.com>
2019-06-16 00:40:54 +0800

    refactor: split components

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Dead simple pixiv collection showcase. Built with Vuetify 2.x and Photoswipe.
 - Setup `yarn`
 - Development `yarn serve`
 - Build `yarn build`
+
+## Thanks
+
+pixiv Collection is made possible by:
+
+- [pixiv API](https://zhuanlan.zhihu.com/p/35243511)
+- [pixiv proxy](https://pixiv.cat/reverseproxy.html)
---
+++ src/components/Dialog.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  v-dialog(v-model="dialog.show" max-width="300"): v-card(v-if="dialog.illust")
+    v-card-title {{ dialog.illust.title  }}
+
+    v-list
+      v-list-item
+        v-tooltip(bottom)
+          template(#activator="{ on }"): v-list-item-avatar: v-btn(
+            v-on="on"
+            :href="`https://www.pixiv.net/member.php?id=${dialog.illust.user.id}`"
+            target="_blank"
+            rel="noreferrer noopener"
+            icon
+          ): v-icon mdi-brush
+
+          span View member on pixiv
+
+        v-list-item-content {{ dialog.illust.user.name }}
+
+      v-list-item
+        v-tooltip(bottom)
+          template(#activator="{ on }"): v-list-item-avatar: v-btn(
+            v-on="on"
+            :href="`https://www.pixiv.net/member_illust.php?mode=medium&illust_id=${dialog.illust.id}`"
+            target="_blank"
+            rel="noreferrer noopener"
+            icon
+          ): v-icon mdi-calendar-blank-outline
+
+          span View illust on pixiv
+
+        v-list-item-content {{ dialog.illust.created_time.substring(0, 16) }}
+
+      v-list-item(v-if="dialog.illust.caption")
+        v-list-item-avatar: v-icon mdi-message-text
+        v-list-item-content {{ dialog.illust.caption }}
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
