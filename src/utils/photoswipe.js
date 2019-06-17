7dfa5723d5880e14527c4be15022c3c5bf9569d5
Kid <kidonng@gmail.com>
2019-06-17 10:12:58 +0800

    refactor: lots of optimaztion and fix
    sweet

--- src/utils/photoswipe.js
+++ src/utils/photoswipe.js
@@ -2,41 +2,30 @@ import PhotoSwipe from 'photoswipe'
 import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default'
 import image from './image'
 
-const init = (items, el, galleryUID, index) =>
-  new PhotoSwipe(el, PhotoSwipeUI, items, {
-    galleryUID,
-    index,
-    barsSize: { top: 0, bottom: 0 },
-    captionEl: false,
-    shareEl: false
-  }).init()
-
-export default (illust, el, index) => {
-  if (illust.metadata) {
-    let items = []
-
-    illust.metadata.pages.forEach(page => {
-      items.push({
-        src: image(page.image_urls.large).original,
-        msrc: image(page.image_urls.large).small,
-        h: page.height,
-        w: page.width
-      })
-
-      if (items.length === illust.metadata.pages.length)
-        init(items, el, illust.id, index)
-    })
-  } else
-    init(
-      [
-        {
-          src: image(illust.image_urls.large).original,
-          msrc: image(illust.image_urls.large).small,
-          h: illust.height,
-          w: illust.width
-        }
-      ],
-      el,
-      illust.id
-    )
-}
+export default (illust, el, index) =>
+  new PhotoSwipe(
+    el,
+    PhotoSwipeUI,
+    illust.metadata
+      ? illust.metadata.pages.map(page => ({
+          src: image(page.image_urls.large).original,
+          msrc: image(page.image_urls.large).small,
+          h: page.height,
+          w: page.width
+        }))
+      : [
+          {
+            src: image(illust.image_urls.large).original,
+            msrc: image(illust.image_urls.large).small,
+            h: illust.height,
+            w: illust.width
+          }
+        ],
+    {
+      galleryUID: illust.id,
+      index,
+      barsSize: { top: 0, bottom: 0 },
+      captionEl: false,
+      shareEl: false
+    }
+  ).init()
