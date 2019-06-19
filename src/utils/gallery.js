dd154783b122f8e74fb877f8f42aa8f040c262da
Kid <kidonng@gmail.com>
2019-06-20 00:04:31 +0800

    refactor: optimize

--- src/utils/gallery.js
+++ src/utils/gallery.js
@@ -1,8 +1,18 @@
+import config from '../../config'
 import PhotoSwipe from 'photoswipe'
-import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default'
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
+import galite from 'ga-lite'
 import time from './time'
 
-export default (pswp, illust, index, rect) => {
+export default (pswpElement, illust, index, rect) => {
+  if (config.googleAnalyticsID)
+    galite('send', {
+      hitType: 'event',
+      eventCategory: illust.user.name,
+      eventAction: 'View',
+      eventLabel: `${illust.title} (${illust.id})`
+    })
+
   const title = `
     <div class="title">${illust.title}</div>
     <div class="subtitle-1 font-weight-bold mb-1">
@@ -24,54 +34,59 @@ export default (pswp, illust, index, rect) => {
     ].join(' #')}</div>
   `
 
-  new PhotoSwipe(
-    pswp,
-    PhotoSwipeUI,
-    illust.meta_pages.length
-      ? illust.meta_pages.map((page, index) => ({
-          src: page.image_urls.original,
-          msrc: page.image_urls.small,
-          h: page.height,
-          w: page.width,
-          title: index === 0 && title
-        }))
-      : [
-          {
-            src: illust.meta_single_page.original_image_url,
-            msrc: illust.image_urls.small,
-            h: illust.height,
-            w: illust.width,
-            title
-          }
-        ],
-    {
-      index,
-      getThumbBoundsFn: () =>
-        rect && { x: rect.left, y: rect.top + window.scrollY, w: rect.width },
-      showHideOpacity: true,
-      galleryUID: illust.id,
-      barsSize: { top: 0, bottom: 0 },
-      loadingIndicatorDelay: 0,
-      shareButtons: [
+  const items = illust.meta_pages.length
+    ? illust.meta_pages.map((page, index) => ({
+        src: page.image_urls.original,
+        msrc: page.image_urls.small,
+        h: page.height,
+        w: page.width,
+        title: index === 0 && title
+      }))
+    : [
         {
-          id: 'download',
-          label: 'View on pixiv',
-          url: `https://www.pixiv.net/member_illust.php?mode=medium&illust_id=${illust.id}`,
-          download: true
-        },
-        {
-          id: 'download',
-          label: 'View illustrator profile',
-          url: `https://www.pixiv.net/member.php?id=${illust.user.id}`,
-          download: true
-        },
-        {
-          id: 'download',
-          label: 'Raw image',
-          url: '{{raw_image_url}}',
-          download: true
+          src: illust.meta_single_page.original_image_url,
+          msrc: illust.image_urls.small,
+          h: illust.height,
+          w: illust.width,
+          title
         }
       ]
-    }
-  ).init()
+
+  const options = {
+    index,
+    getThumbBoundsFn: () =>
+      rect && {
+        x: rect.left,
+        y: rect.top + window.pageYOffset,
+        w: rect.width
+      },
+    showHideOpacity: true,
+    galleryUID: illust.id,
+    // UI Options
+    barsSize: { top: 0, bottom: 0 },
+    loadingIndicatorDelay: 0,
+    shareButtons: [
+      {
+        label: 'View on pixiv',
+        url: `https://www.pixiv.net/member_illust.php?mode=medium&illust_id=${illust.id}`
+      },
+      {
+        label: 'View illustrator profile',
+        url: `https://www.pixiv.net/member.php?id=${illust.user.id}`
+      },
+      { label: 'Raw image', url: '{{raw_image_url}}' }
+    ].map(btn => ({ id: 'download', download: true, ...btn }))
+  }
+
+  const pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options)
+  pswp.listen(
+    'initialZoomIn',
+    () =>
+      (document.title = `${illust.title} / ${illust.user.name} - ${config.title}`)
+  )
+  pswp.listen('destroy', () =>
+    // The title displayed in Chrome (other browsers not tested) will revert if there's no delay
+    setTimeout(() => (document.title = config.title), 50)
+  )
+  pswp.init()
 }
