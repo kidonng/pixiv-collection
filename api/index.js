9235d44219b0a925759ca42af2a7a2b655501f36
Kid <kidonng@gmail.com>
2019-06-19 17:48:59 +0800

    feat: better API

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
-module.exports = async (req, res) => {
-  res.writeHead(200, { 'content-type': 'application/json' })
-  res.end(
-    JSON.stringify(
-      await require('probe-image-size')(
-        require('querystring').parse(
-          req.url.substring(req.url.indexOf('?') + 1)
-        )
-      )
-    )
-  )
-}
+const querystring = require('querystring')
+const probe = require('probe-image-size')
+
+require('http')
+  .createServer(async (req, res) => {
+    const query = querystring.parse(req.url.substring(req.url.indexOf('?') + 1))
+    const reply = data => res.end(JSON.stringify(data))
+
+    if (query.url) {
+      try {
+        reply(await probe(query.url))
+      } catch (e) {
+        res.writeHead(500)
+        reply({ error: JSON.stringify(e) })
+      }
+    } else {
+      res.writeHead(400)
+      reply({ error: 'PARAMETER_NOT_FOUND' })
+    }
+  })
+  .listen(10000, 'localhost')
