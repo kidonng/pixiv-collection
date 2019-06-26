60e0a32c62d4b04e0f4e4ad7f930e1a986368c54
Kid <kidonng@gmail.com>
2019-06-26 19:56:31 +0800

    fix: fix API reply and use Now helpers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import config from '../config'
 import Vue from 'vue'
 import vuetify from './plugins/vuetify'
+import galite from 'ga-lite'
 import App from './App'
+
+if (config.googleAnalyticsID) {
+  galite('create', config.googleAnalyticsID, 'auto')
+  galite('send', 'pageview')
+}
 
 new Vue({
   vuetify,
