c8922a32bbc1df6b923bec278eb4cb4dbba921a1
Kid <kidonng@gmail.com>
2019-07-06 16:22:08 +0800

    refactor: remove pug

--- api/index.js
+++ api/index.js
@@ -4,14 +4,15 @@ const probe = require('probe-image-size')
 module.exports = async (req, res) => {
   if (req.query.id)
     try {
-      const parsed = JSON.parse(
-        (await got(
-          `https://api.imjad.cn/pixiv/v2/${req.url.substring(
-            req.url.indexOf('?')
-          )}`
-        )).body.replace(/i\.pximg\.net/g, 'i.pixiv.cat')
-      )
+      const body = (await got(
+        `https://api.imjad.cn/pixiv/v2/${req.url.substring(
+          req.url.indexOf('?')
+        )}`
+      )).body
+      // Use image proxy
+      const parsed = JSON.parse(body.replace(/i\.pximg\.net/g, 'i.pixiv.cat'))
 
+      // Add small size avatar
       if (parsed.illust || parsed.user) {
         const target = parsed.illust || parsed
         target.user.profile_image_urls.small = target.user.profile_image_urls.medium.replace(
@@ -25,6 +26,7 @@ module.exports = async (req, res) => {
           ? [parsed.illust, ...parsed.illust.meta_pages]
           : [parsed.illust]
 
+        // Add small size image & thumbnail
         illusts.forEach(async illust => {
           illust.image_urls.small = illust.image_urls.medium.replace(
             '540x540_70',
@@ -35,11 +37,12 @@ module.exports = async (req, res) => {
             '250x250_80_a2'
           )
 
+          // Add image height & width
           if (!illust.height) {
-            const res = await probe(illust.image_urls.original)
+            const img = await probe(illust.image_urls.original)
 
-            illust.height = res.height
-            illust.width = res.width
+            illust.height = img.height
+            illust.width = img.width
           }
 
           if (illusts.every(illust => illust.height)) res.json(parsed)
